605cb35b91f16152c097b63e2ef160e1370f56ae
Kaarstthenn-21 <kaarstthen.ancco@tecsup.edu.pe>
2022-05-29 19:24:56 -0500

    Lab 11 parte 2

---
+++ SchoolDAL/CPersonDAL.cs
@@ -0,0 +1,26 @@
+﻿using SchoolEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolDAL
+{
+    public class CPersonDAL
+    {
+        public List<CPerson> Listar()
+        {
+            using(SchoolEntities contexto = new SchoolEntities())
+            {
+                var query = contexto.Person.Select(p => new CPerson
+                {
+                    PersonId = p.PersonID,
+                    LastName = p.LastName,
+                    FirstName = p.FirstName
+                });
+                return query.ToList();
+            }
+        }
+    }
+}
